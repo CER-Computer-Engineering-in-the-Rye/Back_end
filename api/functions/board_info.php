da1b2edc070df0e45dfaf34a815483ec2f651d1e
ryula <hobby519@naver.com>
2021-05-21 13:17:22 +0900

    게시글 정보 불러오기

--- api/functions/board_info.php
+++ api/functions/board_info.php
@@ -2,13 +2,10 @@
 
 $select_query = 'select *from board order by idx';
 $result_query = mysqli_query($conn, $select_query);
-//$result_model = $result_query->fetch_array();
-//$row=mysqli_fetch_array($result_query);
-//if ($result_model != 0) {
-$list_data=[];
+$list_data=['item'=>[]];
         
 while($result_model = $result_query->fetch_array()){
-    array_push($list_data, 
+    array_push($list_data['item'],
         array('idx' => $result_model['idx'],
             'subject' => $result_model['subject'],
             'writer' => $result_model['writer'],
