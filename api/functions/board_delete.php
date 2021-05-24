2aa78036ac729da50c274d7b8698a203b7ab592c
ryula <hobby519@naver.com>
2021-05-25 00:20:06 +0900

    예외처리

--- api/functions/board_delete.php
+++ api/functions/board_delete.php
@@ -3,8 +3,15 @@ $idx=$_POST['idx'];
 $select_query = 'delete from board where idx="'.$idx.'"';
 $result_query = mysqli_query($conn, $select_query);
 
+
 if($result_query){
     echo json_encode(array('result' => true));
+}
+else{
+    // 쿼리 실패
+	header("HTTP/1.1 500 Internal Server Error");
+	echo json_encode(array('message' => '정보를 불러오지 못했습니다.'));
+	exit();
 }
 mysqli_close($conn);
 ?>
