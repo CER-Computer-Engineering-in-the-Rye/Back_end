290f1cc7b0f4e433fa5fb04ba5989f2bf4656148
ryula <hobby519@naver.com>
2021-05-25 00:32:52 +0900

    수정

--- api/functions/board_update.php
+++ api/functions/board_update.php
@@ -21,5 +21,11 @@ $result_query = mysqli_query($conn, $select_query);
 if($result_query){
     echo json_encode(array('result' => true));
 }
+else{
+    // 쿼리 실패
+	header("HTTP/1.1 500 Internal Server Error");
+	echo json_encode(array('message' => '업데이트 하는데 실패했습니다.'));
+	exit();
+}
 mysqli_close($conn);
 ?>
