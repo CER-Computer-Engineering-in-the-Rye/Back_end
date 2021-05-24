9fe99f88beb349ec69bfd30181610694c2ec0150
ryula <hobby519@naver.com>
2021-05-24 15:51:44 +0900

    공백 예외처리

--- api/functions/board_write.php
+++ api/functions/board_write.php
@@ -3,6 +3,16 @@
     $writer=$_POST['writer'];
     $content=$_POST['cnt'];
 
+    if($subject==""){
+        echo json_encode(array('message'=>'제목이 비어있습니다'));
+        exit();
+    }
+    if($content==""){
+        echo json_encode(array('message'=>'내용이 비어있습니다'));
+        exit();
+    }
+    
+
     $select_query = 'insert into board(subject, writer, content, date) values("'.$subject.'", "'.$writer.'", "'.$content.'", now());';
     $result_query = mysqli_query($conn, $select_query);
     if ($result_query) {
