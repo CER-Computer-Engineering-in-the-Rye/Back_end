2487ddc4d61d6a92717fe1be49a2d5804e231e19
sehvdi <eric250@naver.com>
2021-05-25 23:31:08 +0900

    add: CORS setting

--- api/db_info.php
+++ api/db_info.php
@@ -3,4 +3,5 @@
 $conn = mysqli_connect(HOST, USERNAME, PASSWORD, DB_NAME);
 
 header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Origin: *');
 header('HTTP/1.1 200 OK');
