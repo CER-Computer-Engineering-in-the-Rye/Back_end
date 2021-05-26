711086152170e5d625d5dee5c8e687b641dfed09
sehvdi <eric250@naver.com>
2021-05-27 04:34:17 +0900

    add: board_info & delete refactor: uri

--- html/functions/board_delete.php
+++ html/functions/board_delete.php
@@ -1,17 +1,14 @@
 <?php
-$idx=$_GET['idx'];
+
 $select_query = 'delete from board where idx="'.$idx.'"';
 $result_query = mysqli_query($conn, $select_query);
 
-
-if($result_query){
-    echo json_encode(array('result' => true));
-}
-else{
+if(!$result_query){
     // 쿼리 실패
 	header("HTTP/1.1 500 Internal Server Error");
 	echo json_encode(array('message' => '삭제하는데 실패했습니다.'));
 	exit();
 }
+
 mysqli_close($conn);
 ?>
